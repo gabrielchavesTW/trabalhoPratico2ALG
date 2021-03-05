0b705376ec8ee3bded18590fed3e4c16a078a9ae
Gabriel Chaves <78745759+gabrielchavesTW@users.noreply.github.com>
2021-03-05 16:15:38 -0300

    novo algoritmo para detectar ciclos no grafo

--- graps.cpp
+++ graps.cpp
@@ -1,126 +1,129 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
-
-// Data structure to store a graph edge
-class Edge
-{
-public:
-    int src, dest;
-    Edge(int src, int dest)
-    {
-        this->src = src;
-        this->dest = dest;
-    }
-};
-
-// A class to represent a graph object
-class Graph
-{
-public:
-    // a vector of vectors to represent an adjacency list
-    vector<vector<int> > adjList;
-
-    // Graph Constructor
-    Graph(vector<Edge> edges, int N)
-    {
-        // resize the vector to hold `N` elements of type `vector<int>`
-        adjList.resize(N);
-
-        // add edges to the undirected graph
-        for (int i = 0; i < edges.size(); i++)
-        {
-            Edge edgeAtual = edges.at(i);
-            adjList[edgeAtual.src].push_back(edgeAtual.dest);
-            adjList[edgeAtual.dest].push_back(edgeAtual.src);
-        }
-    }
-};
-
-// Node to store vertex and its parent info in BFS
-class Node
-{
-public:
-    int v, parent;
-    Node(int v, int parent)
-    {
-        this->v = v;
-        this->parent = parent;
-    }
-};
-
-// Perform BFS on the graph starting from vertex `src` and
-// return true if a cycle is found in the graph
-bool BFS(Graph const &graph, int src, int N)
-{
-    // to keep track of whether a vertex is discovered or not
-    vector<bool> discovered(N);
-
-    // mark the source vertex as discovered
-    discovered[src] = true;
-
-    // create a queue for doing BFS and
-    // enqueue source vertex
-    queue<Node> q;
-    q.push(Node(src, -1));
-
-    // loop till queue is empty
-    while (!q.empty())
-    {
-        // dequeue front node and print it
-        Node node = q.front();
-        q.pop();
-
-        for (int i = 0; i < graph.adjList[node.v].size(); i++)
-        {
-            int u = graph.adjList[node.v].at(i);
-            if (!discovered[u])
-            {
-                // mark it as discovered
-                discovered[u] = true;
-
-                // construct the queue node containing info
-                // about vertex and enqueue it
-                q.push(Node(u, node.v));
-            }
-
-            // `u` is discovered, and `u` is not a parent
-            else if (u != node.parent)
-            {
-                // we found a cross-edge, i.e., the cycle is found
-                return true;
-            }
-        }
-    }
-
-    // no cross-edges were found in the graph
-    return false;
-}
-
-int main()
-{
-    // initialize edges as per the above diagram
-    vector<Edge> edges;
-    edges.push_back(Edge(2, 4));
-    edges.push_back(Edge(2, 3));
-    edges.push_back(Edge(4, 3));
-
-    // total number of nodes in the graph
-    int N = 5;
-
-    // build a graph from the given edges
-    Graph graph(edges, N);
-
-    // Perform BFS traversal in connected components of a graph
-    if (BFS(graph, 1, N))
-    {
-        cout << "The graph contains a cycle";
-    }
-    else
-    {
-        cout << "The graph doesn't contain any cycle";
-    }
-
-    return 0;
-}
+// A C++ Program to detect  
+// cycle in an undirected graph 
+#include<iostream> 
+#include <list> 
+#include <limits.h> 
+using namespace std; 
+  
+// Class for an undirected graph 
+class Graph 
+{ 
+      
+    // No. of vertices 
+    int V;    
+    
+    // Pointer to an array  
+    // containing adjacency lists 
+    list<int> *adj;   
+    bool isCyclicUtil(int v, bool visited[],  
+                              int parent); 
+public: 
+    
+    // Constructor 
+    Graph(int V);    
+    
+    // To add an edge to graph 
+    void addEdge(int v, int w);  
+    
+    // Returns true if there is a cycle 
+    bool isCyclic();   
+}; 
+  
+Graph::Graph(int V) 
+{ 
+    this->V = V; 
+    adj = new list<int>[V]; 
+} 
+  
+void Graph::addEdge(int v, int w) 
+{ 
+      
+    // Add w to v’s list. 
+    adj[v].push_back(w);  
+    
+    // Add v to w’s list. 
+    adj[w].push_back(v); 
+} 
+  
+// A recursive function that  
+// uses visited[] and parent to detect 
+// cycle in subgraph reachable  
+// from vertex v. 
+bool Graph::isCyclicUtil(int v,  
+                bool visited[], int parent) 
+{ 
+      
+    // Mark the current node as visited 
+    visited[v] = true; 
+  
+    // Recur for all the vertices  
+    // adjacent to this vertex 
+    list<int>::iterator i; 
+    for (i = adj[v].begin(); i != 
+                       adj[v].end(); ++i) 
+    { 
+          
+        // If an adjacent is not visited,  
+        //then recur for that adjacent 
+        if (!visited[*i]) 
+        { 
+           if (isCyclicUtil(*i, visited, v)) 
+              return true; 
+        } 
+  
+        // If an adjacent is visited and  
+        // not parent of current vertex, 
+        // then there is a cycle. 
+        else if (*i != parent) 
+           return true; 
+    } 
+    return false; 
+} 
+  
+// Returns true if the graph contains  
+// a cycle, else false. 
+bool Graph::isCyclic() 
+{ 
+      
+    // Mark all the vertices as not  
+    // visited and not part of recursion 
+    // stack 
+    bool *visited = new bool[V]; 
+    for (int i = 0; i < V; i++) 
+        visited[i] = false; 
+  
+    // Call the recursive helper  
+    // function to detect cycle in different 
+    // DFS trees 
+    for (int u = 0; u < V; u++) 
+    { 
+        
+        // Don't recur for u if  
+        // it is already visited 
+        if (!visited[u])  
+          if (isCyclicUtil(u, visited, -1)) 
+             return true; 
+    } 
+    return false; 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    Graph g1(4); 
+    g1.addEdge(1, 2); 
+    g1.addEdge(2, 3); 
+    g1.addEdge(3, 1); 
+    g1.isCyclic()?  
+       cout << "Graph contains cycle\n": 
+       cout << "Graph doesn't contain cycle\n"; 
+  
+    Graph g2(3); 
+    g2.addEdge(0, 1); 
+    g2.addEdge(1, 2); 
+    g2.isCyclic()?  
+       cout << "Graph contains cycle\n": 
+       cout << "Graph doesn't contain cycle\n"; 
+  
+    return 0; 
+} 
