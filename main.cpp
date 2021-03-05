b4b24d659be346cd7f6130366d26ad01c163f31c
Gabriel Chaves <78745759+gabrielchavesTW@users.noreply.github.com>
2021-03-05 10:17:12 -0300

    .

--- main.cpp
+++ main.cpp
@@ -3,64 +3,102 @@
 #include <vector>
 #include <sstream>
 #include <map>
-#include <list> 
-#include <limits.h> 
-#include <bits/stdc++>
+#include <list>
+#include <limits.h>
+#include <queue>
 using namespace std;
 
-void addEdge(vector<int> adj[], int u, int v)
+class Edge
 {
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-}
- 
-bool isCyclicConntected(vector<int> adj[], int s,
-                        int V, vector<bool>& visited)
+public:
+    int src, dest;
+    Edge(int src, int dest)
+    {
+        this->src = src;
+        this->dest = dest;
+    }
+};
+
+// A class to represent a graph object
+class Graph
 {
-    // Set parent vertex for every vertex as -1.
-    vector<int> parent(V, -1);
- 
-    // Create a queue for BFS
-    queue<int> q;
- 
-    // Mark the current node as 
-    // visited and enqueue it
-    visited[s] = true;
-    q.push(s);
- 
-    while (!q.empty()) {
- 
-        // Dequeue a vertex from queue and print it
-        int u = q.front();
-        q.pop();
- 
-        // Get all adjacent vertices of the dequeued
-        // vertex u. If a adjacent has not been visited,
-        // then mark it visited and enqueue it. We also
-        // mark parent so that parent is not considered
-        // for cycle.
-        for (auto v : adj[u]) {
-            if (!visited[v]) {
-                visited[v] = true;
-                q.push(v);
-                parent[v] = u;
-            }
-            else if (parent[u] != v)
-                return true;
+public:
+    // a vector of vectors to represent an adjacency list
+    vector<vector<int> > adjList;
+
+    // Graph Constructor
+    Graph(vector<Edge> edges, int N)
+    {
+        // resize the vector to hold `N` elements of type `vector<int>`
+        adjList.resize(N);
+
+        // add edges to the undirected graph
+        for (int i = 0; i < edges.size(); i++)
+        {
+            Edge edgeAtual = edges.at(i);
+            adjList[edgeAtual.src].push_back(edgeAtual.dest);
+            adjList[edgeAtual.dest].push_back(edgeAtual.src);
         }
     }
-    return false;
-}
- 
-bool isCyclicDisconntected(vector<int> adj[], int V)
+};
+
+// Node to store vertex and its parent info in BFS
+class Node
 {
-    // Mark all the vertices as not visited
-    vector<bool> visited(V, false);
- 
-    for (int i = 0; i < V; i++)
-        if (!visited[i] && isCyclicConntected(adj, i,
-                                         V, visited))
-            return true;
+public:
+    int v, parent;
+    Node(int v, int parent)
+    {
+        this->v = v;
+        this->parent = parent;
+    }
+};
+
+// Perform BFS on the graph starting from vertex `src` and
+// return true if a cycle is found in the graph
+bool BFS(Graph const &graph, int src, int N)
+{
+    // to keep track of whether a vertex is discovered or not
+    vector<bool> discovered(N);
+
+    // mark the source vertex as discovered
+    discovered[src] = true;
+
+    // create a queue for doing BFS and
+    // enqueue source vertex
+    queue<Node> q;
+    q.push(Node(src, -1));
+
+    // loop till queue is empty
+    while (!q.empty())
+    {
+        // dequeue front node and print it
+        Node node = q.front();
+        q.pop();
+
+        for (int i = 0; i < graph.adjList[node.v].size(); i++)
+        {
+            int u = graph.adjList[node.v].at(i);
+            if (!discovered[u])
+            {
+                // mark it as discovered
+                discovered[u] = true;
+
+                // construct the queue node containing info
+                // about vertex and enqueue it
+                q.push(Node(u, node.v));
+            }
+
+            // `u` is discovered, and `u` is not a parent
+            else if (u != node.parent)
+            {
+                // we found a cross-edge, i.e., the cycle is found
+                return true;
+            }
+        }
+    }
+
+    // no cross-edges were found in the graph
     return false;
 }
 
@@ -112,17 +150,24 @@ public:
 
     bool novoTrechoNaoOcasionaUmCiclo(int trechoOrigem, int trechoDestino)
     {
-        int tamanhoGrafo = resultado.size() + 1;
-        Graph g1(tamanhoGrafo);
+        vector<Edge> edges;
+        edges.push_back(Edge(trechoOrigem, trechoDestino));
         for (int i = 0; i < resultado.size(); i++)
         {
             int src = resultado.at(i).trechoOrigem;
             int dest = resultado.at(i).trechoDestino;
-            g1.addEdge(1, 0);
+            edges.push_back(Edge(src, dest));
         }
-        g1.addEdge(4, 2);
 
-        return !(g1.isCyclic());
+        // total number of nodes in the graph
+        int N = 10;
+
+        // build a graph from the given edges
+        Graph grafo = Graph(edges, N);
+
+        bool grafoTemCiclo = (BFS(grafo, 1, N));
+        cout << grafoTemCiclo;
+        return !(grafoTemCiclo);
     }
 
     void calculaRota(vector<Trecho> vetorTrechos, vector<PontoDeInteresse> vetorPontosDeInteresse)
