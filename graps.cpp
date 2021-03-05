13906e84ef5fe17fdcb5c8b3c1004548bb6a393f
Gabriel Chaves <78745759+gabrielchavesTW@users.noreply.github.com>
2021-03-05 09:07:52 -0300

    tentando implementar algoritmo para descobrir ciclo de grafo

---
+++ graps.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// Data structure to store a graph edge
+class Edge
+{
+public:
+    int src, dest;
+    Edge(int src, int dest)
+    {
+        this->src = src;
+        this->dest = dest;
+    }
+};
+
+// A class to represent a graph object
+class Graph
+{
+public:
+    // a vector of vectors to represent an adjacency list
+    vector<vector<int> > adjList;
+
+    // Graph Constructor
+    Graph(vector<Edge> edges, int N)
+    {
+        // resize the vector to hold `N` elements of type `vector<int>`
+        adjList.resize(N);
+
+        // add edges to the undirected graph
+        for (int i = 0; i < edges.size(); i++)
+        {
+            Edge edgeAtual = edges.at(i);
+            adjList[edgeAtual.src].push_back(edgeAtual.dest);
+            adjList[edgeAtual.dest].push_back(edgeAtual.src);
+        }
+    }
+};
+
+// Node to store vertex and its parent info in BFS
+class Node
+{
+public:
+    int v, parent;
+    Node(int v, int parent)
+    {
+        this->v = v;
+        this->parent = parent;
+    }
+};
+
+// Perform BFS on the graph starting from vertex `src` and
+// return true if a cycle is found in the graph
+bool BFS(Graph const &graph, int src, int N)
+{
+    // to keep track of whether a vertex is discovered or not
+    vector<bool> discovered(N);
+
+    // mark the source vertex as discovered
+    discovered[src] = true;
+
+    // create a queue for doing BFS and
+    // enqueue source vertex
+    queue<Node> q;
+    q.push(Node(src, -1));
+
+    // loop till queue is empty
+    while (!q.empty())
+    {
+        // dequeue front node and print it
+        Node node = q.front();
+        q.pop();
+
+        for (int i = 0; i < graph.adjList[node.v].size(); i++)
+        {
+            int u = graph.adjList[node.v].at(i);
+            if (!discovered[u])
+            {
+                // mark it as discovered
+                discovered[u] = true;
+
+                // construct the queue node containing info
+                // about vertex and enqueue it
+                q.push(Node(u, node.v));
+            }
+
+            // `u` is discovered, and `u` is not a parent
+            else if (u != node.parent)
+            {
+                // we found a cross-edge, i.e., the cycle is found
+                return true;
+            }
+        }
+    }
+
+    // no cross-edges were found in the graph
+    return false;
+}
+
+int main()
+{
+    // initialize edges as per the above diagram
+    vector<Edge> edges;
+    edges.push_back(Edge(1, 2));
+    edges.push_back(Edge(1, 3));
+    edges.push_back(Edge(1, 4));
+    edges.push_back(Edge(2, 5));
+    edges.push_back(Edge(2, 6));
+    edges.push_back(Edge(5, 9));
+    edges.push_back(Edge(5, 10));
+    edges.push_back(Edge(4, 7));
+    edges.push_back(Edge(4, 8));
+    edges.push_back(Edge(7, 11));
+    edges.push_back(Edge(7, 12));
+    edges.push_back(Edge(6, 10));
+
+    // total number of nodes in the graph
+    int N = 13;
+
+    // build a graph from the given edges
+    Graph graph(edges, N);
+
+    // Perform BFS traversal in connected components of a graph
+    if (BFS(graph, 1, N))
+    {
+        cout << "The graph contains a cycle";
+    }
+    else
+    {
+        cout << "The graph doesn't contain any cycle";
+    }
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -3,7 +3,66 @@
 #include <vector>
 #include <sstream>
 #include <map>
+#include <list> 
+#include <limits.h> 
+#include <bits/stdc++>
 using namespace std;
+
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+ 
+bool isCyclicConntected(vector<int> adj[], int s,
+                        int V, vector<bool>& visited)
+{
+    // Set parent vertex for every vertex as -1.
+    vector<int> parent(V, -1);
+ 
+    // Create a queue for BFS
+    queue<int> q;
+ 
+    // Mark the current node as 
+    // visited and enqueue it
+    visited[s] = true;
+    q.push(s);
+ 
+    while (!q.empty()) {
+ 
+        // Dequeue a vertex from queue and print it
+        int u = q.front();
+        q.pop();
+ 
+        // Get all adjacent vertices of the dequeued
+        // vertex u. If a adjacent has not been visited,
+        // then mark it visited and enqueue it. We also
+        // mark parent so that parent is not considered
+        // for cycle.
+        for (auto v : adj[u]) {
+            if (!visited[v]) {
+                visited[v] = true;
+                q.push(v);
+                parent[v] = u;
+            }
+            else if (parent[u] != v)
+                return true;
+        }
+    }
+    return false;
+}
+ 
+bool isCyclicDisconntected(vector<int> adj[], int V)
+{
+    // Mark all the vertices as not visited
+    vector<bool> visited(V, false);
+ 
+    for (int i = 0; i < V; i++)
+        if (!visited[i] && isCyclicConntected(adj, i,
+                                         V, visited))
+            return true;
+    return false;
+}
 
 class PontoDeInteresse
 {
@@ -35,51 +94,56 @@ public:
     }
 };
 
-class Resultado
-{
-public:
-    int custoTotal;
-    int atratividadeAgregada;
-    vector<Trecho> trechosResultado;
-    Resultado()
-    {
-        this->custoTotal = 0;
-        this->atratividadeAgregada = 0;
-    }
-    void adicionarCusto(int custo)
-    {
-        this->custoTotal += custo;
-    }
-    void adicionarAtratividade(int atratividadeTrecho)
-    {
-        this->atratividadeAgregada += atratividadeTrecho;
-    }
-    void adicionarTrecho(Trecho trecho)
-    {
-        this->trechosResultado.push_back(trecho);
-    }
-};
-
 class Configuracoes
 {
 public:
-    void imprimirResultado(Resultado resultado, vector<int> qtdTrechosPorPonto)
+    vector<Trecho> resultado;
+    void imprimirResultado()
     {
-        cout << resultado.custoTotal << " " << resultado.atratividadeAgregada << "\n";
-        for (int i = 0; i < qtdTrechosPorPonto.size(); i++)
+        int custoTotal = 0;
+        int atratividadeAgregada = 0;
+        for (int i = 0; i < resultado.size(); i++)
         {
-            cout << qtdTrechosPorPonto.at(i) << " ";
+            custoTotal += resultado.at(i).custo;
         }
-        cout << "\n";
-        for (int i = 0; i < resultado.trechosResultado.size(); i++)
+
+        cout << custoTotal << " " << atratividadeAgregada;
+    }
+
+    bool novoTrechoNaoOcasionaUmCiclo(int trechoOrigem, int trechoDestino)
+    {
+        int tamanhoGrafo = resultado.size() + 1;
+        Graph g1(tamanhoGrafo);
+        for (int i = 0; i < resultado.size(); i++)
         {
-            Trecho trechoAtual = resultado.trechosResultado.at(i);
-            cout << trechoAtual.trechoOrigem << " " << trechoAtual.trechoDestino << " " << trechoAtual.custo << "\n";
+            int src = resultado.at(i).trechoOrigem;
+            int dest = resultado.at(i).trechoDestino;
+            g1.addEdge(1, 0);
         }
+        g1.addEdge(4, 2);
+
+        return !(g1.isCyclic());
     }
 
     void calculaRota(vector<Trecho> vetorTrechos, vector<PontoDeInteresse> vetorPontosDeInteresse)
     {
+        int tamanhoResultado = vetorPontosDeInteresse.size() - 1;
+        int i = 0;
+        do
+        {
+            Trecho trechoAtual = vetorTrechos.at(i);
+
+            if (resultado.size() == 0)
+            {
+                resultado.push_back(trechoAtual);
+            }
+            else if (novoTrechoNaoOcasionaUmCiclo(trechoAtual.trechoOrigem, trechoAtual.trechoDestino))
+            {
+                resultado.push_back(trechoAtual);
+            }
+            i++;
+            //vetorTrechos.erase(vetorTrechos.begin() + i);
+        } while (i <= tamanhoResultado);
     }
 };
 
@@ -140,7 +204,8 @@ vector<Trecho> leEntradaTrechos(int quantidadeDeTrechosPossiveis)
     return vetorTrechos;
 }
 
-bool comparaCustoDosTrechos(Trecho t1, Trecho t2){
+bool comparaCustoDosTrechos(Trecho t1, Trecho t2)
+{
     return (t1.custo < t2.custo);
 }
 
@@ -155,8 +220,9 @@ int main()
 
     //Ordena o grafo pelo peso das arestas assim como o algoritmo de kruskal
     sort(vetorTrechos.begin(), vetorTrechos.end(), comparaCustoDosTrechos);
-    
+
     Configuracoes configuracoes = Configuracoes();
     configuracoes.calculaRota(vetorTrechos, vetorPontosDeInteresse);
+    configuracoes.imprimirResultado();
     return 0;
 }
